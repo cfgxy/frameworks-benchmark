f218cd08348335e4519d8aa1a270ac50b812174a
顾小宇 <cfgxy@qq.com>
2021-03-19 19:08:41 +0800

    use MulanPSL2

--- golang/gin/docker/test.sh
+++ golang/gin/docker/test.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+# Copyright (c) 2021 Gu Xiaoyu
+# Frameworks-Benchmark is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
 ab -c 50 -n 50000 -k http://127.0.0.1:8885/hello
