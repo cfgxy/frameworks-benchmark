9bbe9c6bcb2d162aaeb2cb072eec979ca8b9186e
顾小宇 <cfgxy@qq.com>
2021-03-23 21:33:12 +0800

    test echo with gorm

--- golang/echo/src/Hello.go
+++ golang/echo/src/Hello.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"net/http"
 )
 
@@ -15,6 +18,7 @@ func main()  {
 
 	// Routes
 	e.GET("/hello", hello)
+	e.GET("/gorm", handleGorm)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
@@ -24,4 +28,18 @@ func main()  {
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello World")
+}
+
+func handleGorm(c echo.Context) error {
+	dsn := "root@tcp(192.168.3.8:3306)/ahjob?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err == nil {
+		var t CommonType
+		db.First(&t, 1)
+
+		return c.String(http.StatusOK, fmt.Sprintf("Hello %s, from gorm", t.TypeName))
+	}
+
+	return c.String(http.StatusOK, "Hello")
 }
--- golang/echo/src/go.mod
+++ golang/echo/src/go.mod
@@ -2,4 +2,8 @@ module hello
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.2.1
+require (
+	github.com/labstack/echo/v4 v4.2.1
+	gorm.io/driver/mysql v1.0.5
+	gorm.io/gorm v1.21.4
+)
