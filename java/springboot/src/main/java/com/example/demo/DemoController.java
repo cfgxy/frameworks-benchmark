f3ef0d34e64a2c9d4177b36339b036b56a35edf6
顾小宇 <cfgxy@qq.com>
2021-03-23 22:37:38 +0800

    test jpa on springboot with hikari

--- java/springboot/src/main/java/com/example/demo/DemoController.java
+++ java/springboot/src/main/java/com/example/demo/DemoController.java
@@ -1,14 +1,32 @@
 package com.example.demo;
 
+import com.example.demo.data.CommonType;
+import com.example.demo.data.CommonTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class DemoController {
+
+    @Lazy@Autowired
+    CommonTypeRepository commonTypeRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
 
+    @GetMapping("/jpa")
+    public String jpa() {
+        Optional<CommonType> t = commonTypeRepository.findById(1L);
+        if (t.isEmpty()) {
+            return "Hello";
+        }
+
+        return String.format("Hello %s, from jpa", t.get().getTypeName());
+    }
 }
---
+++ java/springboot/src/main/java/com/example/demo/data/CommonTypeRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommonTypeRepository extends CrudRepository<CommonType, Long> {
+}
